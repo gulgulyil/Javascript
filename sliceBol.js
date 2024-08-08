b6d3690cb461e3cb95aa86daba452da1e0333705
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-08 13:00:50 +0200

    Create sliceBol.js

---
+++ sliceBol.js
@@ -0,0 +1,2 @@
+console.log([0,1,2,3,4].slice(2,4)); // -> [2,3] slice listeyi boler 2. ve 3. siradaki elemanlari verir. 4. dahil etmez
+console.log([0,1,2,3,4].slice(2)); //-> [2,3,4] 2.yi ve sonrasini listeler
