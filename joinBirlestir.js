41b5f8b6d5dcf44ecc24e3b7dd42f5410df87034
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-08 12:43:40 +0200

    Create joinBirlestir.js

---
+++ joinBirlestir.js
@@ -0,0 +1,4 @@
+let cumle = "JavaScripti Turkce ogrenmek harika";
+let kelime = cumle.split(" "); //split ile kelimelere ayiriyor dizi olusturuyor
+console.log(kelime);
+console.log(kelime.join(". "));// join ile birlestirip aralarina . koyarak liste haline getiriyor
