9db2ffea9a933eb802c589b28cfd2e8a29d0a9ae
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-08 13:00:10 +0200

    Create concatBirlestir.js

---
+++ concatBirlestir.js
@@ -0,0 +1,7 @@
+const array1 = ['a','b','c'];
+console.log(array1);
+const array2 = ['d', 'e', 'f'];
+console.log(array2);
+const array3 = array1.concat(array2); //birlestirir [a,b,c,d,e,f]
+console.log(array3);
+console.log(array3.slice(2,5));//[c,d,e]
