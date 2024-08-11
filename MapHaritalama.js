d8d1acb871fd5f4956e90898e47bfa9a71a47b02
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-11 11:31:04 +0200

    Create MapHaritalama.js

---
+++ MapHaritalama.js
@@ -0,0 +1,7 @@
+let yas = new Map();
+yas.set("Baris", 39);
+yas.set("Leyla", 22);
+yas.set("Hulya", 62);
+console.log("Hulya", yas.get("Hulya"), "yasinda"); //
+console.log(yas.has("Osman")); //
+console.log(yas.has("toString"));
