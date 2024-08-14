f3c1d24b22ae72547613b46423c6818933206f30
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-14 14:30:59 +0200

    Update encapsulaysinTavsan.js

--- encapsulaysinTavsan.js
+++ encapsulaysinTavsan.js
@@ -1,1 +1,14 @@
 
+function konus(hat){
+    console.log(this.type, "tavsan diyor ki:", hat);
+}
+
+let beyazTavsan = {
+    type: "Beyaz", konus
+};
+let acTavsan = {
+    type: "Ac", konus
+};
+beyazTavsan.konus("Kulaklarim ve biyiklarim askina, " + "be kadar gec oluyor");
+acTavsan.konus("Su an havuc yiyebilirdim");
+konus.call(acTavsan, "cokkkkk acimmmmm :(");
