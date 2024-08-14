11f7aa29da371d17cb22f6c5978a8f3d4ff983bf
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-14 14:25:21 +0200

    Create typeOf.js

---
+++ typeOf.js
@@ -0,0 +1,7 @@
+//Prototipleme / Prototypes
+let bosObje = 5;
+let obje = {};
+console.log(typeof obje); //object
+console.log(typeof bosObje); //nummer
+console.log(typeof bosObje.toString); //function
+console.log(typeof bosObje.toString())  //string
