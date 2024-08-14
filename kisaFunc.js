d4317ab1d6a51c22c71dd0f7e42c3f02ef686c44
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-14 13:52:10 +0200

    Create kisaFunc.js

---
+++ kisaFunc.js
@@ -0,0 +1,10 @@
+hello = () => {
+    return ' Hello World!';
+}
+console.log(hello());
+//() => ısaretı fonksıyon tanımlandıgını soyluyor
+
+helloT = () => 'Selamlar';
+console.log(helloT());
+
+//daha kısa bır yapıda kullanmamızı saglar
