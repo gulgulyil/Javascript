ea823265786438d370c867c6db9dc852a89e6550
Gülnur Yildiz <72104981+gulgulyil@users.noreply.github.com>
2024-08-14 14:26:07 +0200

    Update usluIfade.js

--- usluIfade.js
+++ usluIfade.js
@@ -1,1 +1,10 @@
+function usluIfade(taban, ust = 2){
+    let sonuc = 1;
+    for (let x = 0; x < ust; x++){
+        sonuc *= taban;
+    }
+    return sonuc;
+}
 
+console.log(usluIfade(4));
+console.log(usluIfade(2,6));
